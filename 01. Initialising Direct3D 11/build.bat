0d77b1ef53e2a6dac83b9144c86927d45db77967
kevinmoran <kevinmoran007@gmail.com>
2019-06-25 20:39:11 +0100

    Added debug/release configs in build.bats

--- 01. Initialising Direct3D 11/build.bat
+++ 01. Initialising Direct3D 11/build.bat
@@ -8,7 +8,13 @@ REM call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-set COMPILER_FLAGS=/MTd /nologo /Gm- /EHa- /GR- /fp:fast /Od /Oi /W4 /wd4201 /wd4100 /wd4189 /wd4514 /wd4820 /wd4505 /FC /Fm /Z7
+set COMMON_COMPILER_FLAGS=/nologo /EHa- /GR- /fp:fast /Oi /W4 /wd4100 /FC /Fm
+
+set DEBUG_FLAGS=/DDEBUG_BUILD /Od /MTd /Z7
+set RELEASE_FLAGS =/O2
+
+set COMPILER_FLAGS=%COMMON_COMPILER_FLAGS% %DEBUG_FLAGS%
+REM set COMPILER_FLAGS=%COMMON_COMPILER_FLAGS% %RELEASE_FLAGS%
 
 set LINKER_FLAGS=/INCREMENTAL:NO /opt:ref
 set SYSTEM_LIBS=user32.lib gdi32.lib winmm.lib d3d11.lib
