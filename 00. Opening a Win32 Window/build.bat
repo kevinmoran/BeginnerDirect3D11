2b7c7b8ce9480b72cba1120c90355dde8ea27790
Anton Gerdelan <capnramses@users.noreply.github.com>
2019-10-21 17:30:32 +0100

    added VS 2019 Community path
    tested on Windows 10

--- 00. Opening a Win32 Window/build.bat
+++ 00. Opening a Win32 Window/build.bat
@@ -7,6 +7,7 @@ REM call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" 
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" x64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 set COMPILER_FLAGS=/MTd /nologo /Gm- /EHa- /GR- /fp:fast /Od /Oi /W4 /wd4201 /wd4100 /wd4189 /wd4514 /wd4820 /wd4505 /FC /Fm /Z7
 
